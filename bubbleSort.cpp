e119364924c2cee12b90b852be56f2edac61e2b0
m666362 <mdrayhan9464@gmail.com>
2021-07-26 22:43:04 +0600

    array len

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -7,17 +7,16 @@ void printArray(int nums[], int size);
 int main()
 {
     int nums[] = { 95, 50, 80, 20, 30, 40 };
-    for(int i=5; i>=1; i--){
+    int len = sizeof(nums)/sizeof(nums[0]);
+    for(int i=(len-1); i>=1; i--){
         for(int j=0; j<=i-1; j++){
             if(nums[j]>nums[j+1])
                 swap(nums, j, j+1);
         }
-        printArray(nums, 6);
     }
     printArray(nums, 6);
     return 0;
 }
-
 
 void swap(int nums[], int lowPosition, int highPosition){
     int temp = nums[lowPosition];
